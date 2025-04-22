bc52c0e750c57c2491b96f15096e3cc9c46fb2e0
Elena Kolbanova <gpjsdjts@gmail.com>
2025-04-22 18:29:19 -0300

    feat(test): added patrol, upped versions
    Upgraded kotlin & android & gradle to fix local build issued. Fixed JavaVersion, kotlin options. Added patrol to dependencies.

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 1
 /// Strings: 10
 ///
-/// Built on 2024-12-28 at 02:56 UTC
+/// Built on 2025-04-22 at 19:43 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
