064f7de1a029bf80a78020dc27ce4998e945d950
Elena Kolbanova <gpjsdjts@gmail.com>
2025-04-23 17:29:19 -0300

    fix(test): fix ios fb test
    Changed device to iphone15, debug to release

--- integration_test/scripts/fb_test_lab/fb_run_dummy_ios_test.sh
+++ integration_test/scripts/fb_test_lab/fb_run_dummy_ios_test.sh
@@ -1,4 +1,4 @@
-# execute in root
+# invoke from project root using absolute path
 
 # set up patrol cli
 dart pub global activate patrol_cli   
@@ -12,16 +12,17 @@ gcloud --quiet config set project buenro-hotel-search
 # build Runner and RunnerUITest, 
 # FB test lab does not support simulators, so you would've to build on physical device in release mode 
 # (you must have codesigning rights)
-patrol build ios --target integration_test/dummy_test.dart --release
+patrol build ios --target integration_test/dummy_test.dart --release --verbose
 
 # archieve
-# Depending on the system Debug-iphonesimulator Runner_iphonesimulator target paths may differ
+# Depending on the system Release-iphoneos Runner_iphoneos target paths may differ
 # may need to copy from previous command output
 pushd build/ios_integ/Build/Products
-zip -r ios_tests.zip Release-iphoneos Runner_iphoneos18.2-arm64-x86_64.xctestrun
+zip -r ios_tests.zip Release-iphoneos Runner_iphoneos18.2-arm64.xctestrun
 popd
+
 
 #upload to fb test lab
 gcloud firebase test ios run \
   --test build/ios_integ/Build/Products/ios_tests.zip \
-  --device model=iphone8,version=18.0,locale=en_US,orientation=portrait
+  --device model=iphone15,version=18.0,locale=en_US,orientation=portrait
