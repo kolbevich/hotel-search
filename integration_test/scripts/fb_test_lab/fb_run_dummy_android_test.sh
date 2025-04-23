7ae5e971fd46c90cd159193bc777a636fe61ad06
Elena Kolbanova <gpjsdjts@gmail.com>
2025-04-23 11:13:09 -0300

    feat(test): add gcloud set up to fb scripts

--- integration_test/scripts/fb_test_lab/fb_run_dummy_android_test.sh
+++ integration_test/scripts/fb_test_lab/fb_run_dummy_android_test.sh
@@ -1,3 +1,14 @@
+# execute in root
+
+# set up patrol cli
+dart pub global activate patrol_cli   
+export PATH="$PATH":"$HOME/.pub-cache/bin"
+
+# set up gcloud cli
+gcloud auth activate-service-account --key-file=<my_service_account>
+gcloud --quiet config set project buenro-hotel-search
+
+
 # build app-debug.apk and app-debug-androidTest.apk
 patrol build android --target integration_test/dummy_test.dart
 
--- integration_test/scripts/fb_test_lab/fb_run_dummy_ios_test.sh
+++ integration_test/scripts/fb_test_lab/fb_run_dummy_ios_test.sh
@@ -1,6 +1,17 @@
-# in root 
+# execute in root
 
-# build Runner and RunnerUITest, (you must have codesigning rights)
+# set up patrol cli
+dart pub global activate patrol_cli   
+export PATH="$PATH":"$HOME/.pub-cache/bin"
+
+# set up gcloud cli
+gcloud auth activate-service-account --key-file=<my_service_account>
+gcloud --quiet config set project buenro-hotel-search
+
+
+# build Runner and RunnerUITest, 
+# FB test lab does not support simulators, so you would've to build on physical device in release mode 
+# (you must have codesigning rights)
 patrol build ios --target integration_test/dummy_test.dart --release
 
 # archieve
