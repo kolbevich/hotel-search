a6de989545702a3e680e1362f126118b48020eb7
Elena Kolbanova <gpjsdjts@gmail.com>
2025-04-23 21:13:53 -0300

    fix(test): add getit and hive configuration

--- integration_test/utils/test_extension.dart
+++ integration_test/utils/test_extension.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hive/hive.dart';
+import 'package:hotel_booking/core/di/injectable.dart';
+import 'package:hotel_booking/i18n/strings.g.dart';
 import 'package:hotel_booking/main.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:patrol/patrol.dart';
 
 import '../screens/overview_screen.dart';
 
 extension TestExtension on PatrolIntegrationTester {
   Future<OverviewScreen> openApp() async {
+    Hive.defaultDirectory = (await getApplicationDocumentsDirectory()).path;
+    configureDependencies();
     await pumpWidget(App(), const Duration(seconds: 5));
 
     await pump(const Duration(seconds: 2));
