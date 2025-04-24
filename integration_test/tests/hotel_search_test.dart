c83e572eb5a139dce861d03c37547d78dd6a3d66
Elena Kolbanova <gpjsdjts@gmail.com>
2025-04-23 21:54:19 -0300

    feat(test): added hotel search test
    This test checks if search results appear on entering text in the input area

---
+++ integration_test/tests/hotel_search_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:patrol/patrol.dart';
+
+import '../screens/base_screen.dart';
+import '../utils/test_extension.dart';
+
+void main() {
+  patrolTest("I open Hotels tab, enter text in the search bar and see search results", ($) async {
+    await $.openApp();
+    final tabs = TabBar($);
+
+    await $.wait(seconds: 5);
+
+    final hotelsScreen = await tabs.openHotelsScreen();
+    await hotelsScreen.searchBarEnter(text: "London");
+
+    final isResultEmpty = await hotelsScreen.isSearchResultEmpty();
+
+    expect(isResultEmpty, false, reason: "Expected search result with hotels");
+  });
+}
